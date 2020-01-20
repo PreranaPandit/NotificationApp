6c7aee108a53916e58b8f1259691a22828850bed
PreranaPandit <preranapandit07@gmail.com>
2020-01-20 10:59:13 +0545

    Notification high/low added

--- app/src/main/java/com/example/prerana/notificationapp/MainActivity.java
+++ app/src/main/java/com/example/prerana/notificationapp/MainActivity.java
@@ -1,23 +1,75 @@
 package com.example.prerana.notificationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.example.prerana.notificationapp.createchannel.CreateChannel;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnnotification_one, btnnotification_two;
+    private NotificationManagerCompat notificationManagerCompat;
+
+    private Button btnnotification_one, btnnotification_two;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+        CreateChannel channel = new CreateChannel(this);
+        channel.createChannel();
+
         btnnotification_one = findViewById(R.id.notification_one);
         btnnotification_two = findViewById(R.id.notification_two);
 
+        btnnotification_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                DisplayNotification();
 
+            }
+        });
+
+        btnnotification_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DisplayNotification2();
+            }
+        });
+
+    }
+
+    private void DisplayNotification2() {
+
+        Notification notification = new NotificationCompat.Builder(this, CreateChannel.CHANNEL_2)
+                .setSmallIcon(R.drawable.ic_message_black_24dp)
+                .setContentTitle("Message")
+                .setContentText("Message is here!.")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManagerCompat.notify(2,notification);
+    }
+
+    private void DisplayNotification() {
+
+        Notification notification = new NotificationCompat.Builder(this,CreateChannel.CHANNEL_1)
+
+                .setSmallIcon(R.drawable.ic_call_black_24dp)
+                .setContentTitle("First call")
+                .setContentText("Sangita, Tasmiya, Ishika called Prerana")
+                .setCategory(NotificationCompat.CATEGORY_CALL)
+                .build();
+
+        notificationManagerCompat.notify(1, notification);
     }
 }
